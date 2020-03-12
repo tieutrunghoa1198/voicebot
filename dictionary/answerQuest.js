765907f0eefffc0eaa1535ca48e2de45d7a3f07e
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-03-12 21:15:33 +0700

    commit

--- dictionary/answerQuest.js
+++ dictionary/answerQuest.js
@@ -10,11 +10,11 @@ const answer = (question) => new Promise((resolve, reject) => {
     else if ( question === questData[2].question) {
         resolve('Giờ làm việc của vietcombank từ sáng tới khi nào chán thì đóng cửa nhé')
     }
-    else if ( question === questData[3].question) {
+    else if ( question === questData[3].question || question === questData[10].question) {
         resolve('Bạn bao nhiêu tuổi nhỉ ?')
     }
     else if ( question === questData[4].question) {
-        resolve('Chà, có vẻ như bạn chưa đủ độ tuổi để ăn chơi òi, hẹn bạn 1 mùa xuân khác nhé.')
+        resolve('Chà, có vẻ như bạn chưa đủ điều kiện để có thể vay rồi.')
     }
     else if ( question === questData[5].question) {
         resolve('Bạn cần phô tô chứng minh thư hoặc hộ khẩu, chứng minh công việc hiện tại và thu nhập của mình nhé.')
@@ -22,7 +22,7 @@ const answer = (question) => new Promise((resolve, reject) => {
     else if ( question === questData[6].question) {
         resolve('Bạn cần Hợp đồng lao động và xác nhận công tác hoặc Bảo hiểm y tế nhé.')
     }
-    else if ( question === questData[7].question) {
+    else if ( question === questData[7].question || question === questData[9].question) {
         resolve('Bạn cần Sao kê lương, xác nhận tiền mặt 3 tháng gần nhất hoặc hợp đồng bảo hiểm nhân thọ hoặc thẻ tín dụng nhé.')
     }
     else if ( question === questData[8].question) {
